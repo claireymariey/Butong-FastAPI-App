6cbf29354c36026fe8278b104d7e62f2d3525e57
Claire <claire.mb0504@gmail.com>
2025-04-13 15:13:26 +0800

    all changes added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # for local dev
         "https://butong-fastapi-app.netlify.app",
-    ]  # your deployed frontend],  # Change as needed for production
+    ],  # your deployed frontend],  # Change as needed for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
